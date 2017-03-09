97fc361ac0b9b2aca8690c28016e36518d2e85f3
GPad <gpadovani@gmail.com>
2017-03-09 23:31:46 +0100

    Ring size should be power of two

--- config/config.exs
+++ config/config.exs
@@ -38,7 +38,7 @@ config :riak_core,
   handoff_port: 8099,
   handoff_ip: '127.0.0.1',
   schema_dirs: ['priv'],
-  ring_creation_size: 12,
+  ring_creation_size: 16,
   vnode_inactivity_timeout: 10000
 
 config :lager,
